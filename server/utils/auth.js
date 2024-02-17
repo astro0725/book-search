9af7077333c1ef31ec2158c41a09cbe951d0c83e
astro <angelica.strong0725@gmail.com>
2024-02-16 20:11:26 -0800

    updated auth middlewares

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,15 +8,17 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization
+
+    console.log(`authMiddleware invoked with ${req}`);
+
+    if (!token) {
+      return res.status(400).json({ message: 'You have no token!' });
+    }
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
