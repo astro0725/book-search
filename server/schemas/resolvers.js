41274c403916bbe73df792c76ff5b66c947b3c16
astro <angelica.strong0725@gmail.com>
2024-02-16 17:56:21 -0800

    corresponding resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,67 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // get the current user
+    me: async (_, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+  Mutation: {
+    // user login
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    // add a user
+    addUser: async (_, args) => {
+      const user = await User.create(args);
+      if (!user) {
+        throw new Error('Something went wrong!');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    // save a book
+    saveBook: async (_, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        ).populate('savedBooks');
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // remove a book
+    removeBook: async (_, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        ).populate('savedBooks');
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
