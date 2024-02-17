88abce08ed3f44f7146a5a258f4d3ac068acb567
astro <angelica.strong0725@gmail.com>
2024-02-16 19:48:21 -0800

    enclosed db open in the start server function

--- server/server.js
+++ server/server.js
@@ -8,27 +8,35 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// create a new apollo server and pass in our schema data
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-// apply the apollo gql middleware and set the path to /graphql
-server.applyMiddleware({ app });
+async function startServer() {
+  // start the Apollo server
+  await server.start();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  // apply the Apollo GraphQL middleware and set the path to /graphql
+  server.applyMiddleware({ app });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 }
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+// call startServer to boot up the server
+startServer();
